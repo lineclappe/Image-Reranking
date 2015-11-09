172ba64c964829e66c6d4c261f7af35304864557
lineclappe <line_clappe@163.com>
2015-11-09 11:40:22 +0800

    修改计算的公式

--- Data_Analyse/CalculateMAP.m
+++ Data_Analyse/CalculateMAP.m
@@ -2,16 +2,26 @@
 function value = CalculateMAP(rank,data,T)
 num_rel = 0;
 num_img = size(rank,1);
+full_rel =  sum(data(:,3)==1);
 map = 0;
 count = 0;
-T = min(T,num_img);
-for i = 1:T
+level = min(T,num_img);
+for i = 1:level
     seq = rank(i,1);
     rel = data(seq,3);
     count = count + 1;
     num_rel = num_rel + rel;
-    if rel == 1
+    if rel == 1        
         map = map + num_rel/count;
     end
 end
-value = map / num_rel;
+if num_rel == 0
+    value = 0;
+else
+  %   value = map /level;
+    if T >=full_rel
+         value = map / full_rel;
+     else
+         value = map / T;
+     end
+end
